f0b74e94793d850e24b23460386426deb7738c1b
Alexandre Lustosa <lutosaalexandre@gmail.com>
2023-02-05 21:24:08 -0300

    feat: creates event classes

---
+++ SM-Post/Post.Common/Events/CommentAddedEvent.cs
@@ -0,0 +1,22 @@
+﻿using CQRS.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post.Common.Events
+{
+    public class CommentAddedEvent : BaseEvent
+    {
+        public CommentAddedEvent(): base(nameof(CommentAddedEvent))
+        {
+        }
+
+        public Guid CommentId { get; set; }
+
+        public string Comment { get; set; }
+        public string Username { get; set; }
+        public DateTime CommentDate { get; set; }
+    }
+}
