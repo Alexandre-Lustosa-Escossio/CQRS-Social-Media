d26dbea236ae7933d271438c784590dad320b2b5
Alexandre Lustosa <lutosaalexandre@gmail.com>
2023-02-05 21:55:09 -0300

    feat: Creates command dispatcher

---
+++ SM-Post/Post.Cmd/Post.Cmd.Infrastructure/Post.Cmd.Infrastructure/Dispatchers/CommandDispatcher.cs
@@ -0,0 +1,37 @@
+﻿using CQRS.Core.Commands;
+using CQRS.Core.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post.Cmd.Infrastructure.Dispatchers
+{
+    public class CommandDispatcher : ICommandDispatcher
+    {
+        private readonly Dictionary<Type, Func<BaseCommand, Task>> _handlers = new();
+        public void RegisterHandler<T>(Func<T, Task> handler) where T : BaseCommand
+        {
+            if (_handlers.ContainsKey(typeof(T)))
+            {
+                throw new IndexOutOfRangeException("You cannot register the same command handler twice");
+            }
+
+            _handlers.Add(typeof(T), x => handler((T)x));
+;
+        }
+
+        public async Task SendAsync(BaseCommand command)
+        {
+            if (_handlers.TryGetValue(command.GetType(), out Func<BaseCommand, Task> handler))
+            {
+                await handler(command);
+            } 
+            else
+            {
+                throw new ArgumentNullException(nameof(handler), "No command handler was registered!");
+            }
+        }
+    }
+}
